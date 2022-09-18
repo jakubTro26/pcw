3c9e21d868352c2d9190bf19083317084ed4e991
kuba <info@weblead.pl>
2022-09-18 13:47:53 +0200

    commit

--- wp-content/plugins/pc-market/import_one.php
+++ wp-content/plugins/pc-market/import_one.php
@@ -2,7 +2,7 @@
 ini_set('max_execution_time', '600');
 set_time_limit(600);
 
-define( 'WC_ABSPATH', '"/home4/smakolyk/public_html/pcwordpress/wp-content/plugins/woocommerce/"' );
+define( 'WC_ABSPATH', '/home4/smakolyk/public_html/pcwordpress/wp-content/plugins/woocommerce/' );
 define( 'ABSPATH', '/home4/smakolyk/public_html/pcwordpress/' );
 
 	 function do_ajax_product_import($file) {
