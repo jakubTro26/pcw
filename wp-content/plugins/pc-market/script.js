a7d11625e001c1db1f7b36fedf0c6739f88a9d4d
kuba <info@weblead.pl>
2022-09-19 06:38:40 +0200

    commit

--- wp-content/plugins/pc-market/script.js
+++ wp-content/plugins/pc-market/script.js
@@ -18,6 +18,81 @@ jQuery(document).ready(function($){
              console.log(xhr);
             }
             });
+            $.ajax({
+                url: "https://smakolyk.nstrefa.pl/pcwordpress/wp-content/plugins/pc-market/dir.php",
+                cache: false,
+                type: "GET",
+            success: function(response) {
+                console.log(response);
+                
+                },
+            error: function(xhr) {
+                 console.log(xhr);
+                }
+                });
+               
+
+                    $.ajax({
+                        url: "https://smakolyk.nstrefa.pl/pcwordpress/wp-content/plugins/pc-market/echo.php",
+                        data: { 
+                                    "VarA": window.csv 
+            
+                              },
+                        cache: false,
+                        type: "POST",
+                    success: function(response) {
+                        console.log(response);
+                        },
+                    error: function(xhr) {
+                         console.log(xhr);
+                        }
+                        });
+
+                        var main_array = window.csv;
+                        var sub_arrays;
+                        var count_of_arrays=main_array.length/20;
+                        window.super_array=[];
+                    
+                    for(var k=0; k<count_of_arrays;k++)
+                    {
+                        var array_elements=[];
+                        for(var i=0;i<20;i++){
+                
+                             var element = main_array.shift();
+                             if(element!=undefined)
+                             {
+                                var l = array_elements.push(element);
+                             }
+                            if(i==19){
+                                var o = window.super_array.push(array_elements);
+                            }
+                             }
+                    }
+
+                    var super_var = window.super_array;
+
+                    super_var.forEach(element => {
+            
+                        $.ajax({
+                        url: php_vars.ajaxurl,
+                        data: { 
+                                    "VarA": element
+            
+                              },
+                        cache: false,
+                        type: "POST",
+                    success: function(response) {
+                        console.log(response);
+                        
+                        },
+                    error: function(xhr) {
+                         console.log(xhr);
+                        }
+                        });
+                        
+                    });
+                        
+              
 
 
 
@@ -97,6 +172,7 @@ jQuery(document).ready(function($){
              console.log(xhr);
             }
             });
+            
     });
 });
 
